39c7a1dfdfeb96b2c6d995212d7fee02926517ad
Sudipto Das <sudiptodas877@gmail.com>
2023-05-21 08:49:25 +0530

    task added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const HandleInputTask = (event) => {
+    setTask(event.target.value);
+    console.log(task);
+  };
+
+  const handleAddTask = () => {
+    if (task.trim() !== "") {
+      setTodos([...todos,task]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task</h1>
+      <input onChange={HandleInputTask} value={task} />
+      <button onClick={handleAddTask}>AddTask</button>
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
